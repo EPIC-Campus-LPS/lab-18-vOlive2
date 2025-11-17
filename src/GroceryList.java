5b84448e04b5174388a7fa89900d06c33043c8dd
vOlive2.0 <OLiveA148@lpsk12.org>
2025-11-17 10:30:59 -0700

    sure

--- src/GroceryList.java
+++ src/GroceryList.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-import org.jsoup.Jsoup;
+import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
